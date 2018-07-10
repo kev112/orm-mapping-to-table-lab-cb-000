ef8a5bedf88e6a4daef99a01938935ddcf4863f4
Tao <werewaswas@live.com>
2018-07-10 20:17:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
   attr_accessor :name, :grade
 
   def initialize(name:, grade:)
@@ -10,6 +10,6 @@ class Student
   end
 
   def.save
-    
+    self.name, self.grade
   end
 end
